9f8831eef51e7e2e3b9a318055510fdcd07058dc
Rafael Rodrigues <rafael-rodrigues-@live.com>
2024-07-06 11:29:31 -0300

    Remove storyboard

--- desafio-cora/desafio-cora/ViewController.swift
+++ desafio-cora/desafio-cora/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  desafio-cora
-//
-//  Created by Rafael Rodrigues on 06/07/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
